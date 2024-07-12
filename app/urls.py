8ec0309077f32eb39bf12d2a81b4a4a5caf52351
PHGustavo <guhoepers25@gmail.com>
2024-07-12 08:39:31 -0300

    chore: "modelo" finalizado

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet, ModeloViewSet
 
 
 router = DefaultRouter()
@@ -19,6 +19,7 @@ router.register(r"users", UserViewSet, basename="users")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"cores", CorViewSet, basename="cor")
 router.register(r"marcas", MarcaViewSet, basename="marca")
+router.register(r"modelos", ModeloViewSet, basename="modelo")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- core/models/modelo.py
+++ core/models/modelo.py
@@ -1,16 +1,17 @@
 from django.db import models
-from .marca import marca
-from .categoria import categoria
+from .marca import Marca
+from .categoria import Categoria
 
 class Modelo(models.Model):
     descricao = models.CharField(max_length = 100)
 
     marca = models.ForeignKey(
-        marca, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True
+        Marca, on_delete=models.RESTRICT, related_name="modelos", null=True, blank=True
     )
 
-    categoria = models.ForeignKey(categoria, on_delete=models.PROTECT, related_name="veiculos", null=True, blank=True)
-    
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.RESTRICT, related_name="modelos", null=True, blank=True
+    )
+
     def __str__ (self):
         return f"{self.descricao} {(self.id)}"
-    
